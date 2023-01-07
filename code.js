fdcefd7df03ac015d48f47644f5acdb83e481ba9
TheNameIsArt <anya.r.tandrup@gmail.com>
2023-01-07 21:27:04 +0100

    update

--- code.js
+++ code.js
@@ -12,29 +12,4 @@ $(document).ready(function()
 // Calling ScroolTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
 
-// Tabs
-function openPage(evt, pageName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tab-content");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tab-links");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(pageName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-document.getElementById("defaultOpen").click();
-document.getElementById("defaultOpen2").click();
-
-
 
